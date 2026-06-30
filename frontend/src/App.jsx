132c026e8139e6c50775b0b04f721baba87f5878
Albiona Maxhuni <albionamax13@gmail.com>
2026-06-30 18:17:25 +0200

    Complete Albanian UI translation coverage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
+import RuntimeAlbanianTranslator from "./components/RuntimeAlbanianTranslator";
 import "./styles/ui.css";
 import "./styles/theme.css";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <RuntimeAlbanianTranslator />
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
